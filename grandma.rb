d5c533bf6cea8c82851b09561aee8aca8ac8de2b
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-11 17:33:36 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(message)
-  if message != message.capitalize
-    return "HUH?! SPEAK UP, SONNY!"
+  if message == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif message == message.capitalize
     return "NO, NOT SINCE 1938!"
-  elsif message == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+  else
+    
+    return "HUH?! SPEAK UP, SONNY!"
   end
     
 end
